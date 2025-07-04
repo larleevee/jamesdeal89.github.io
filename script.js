13feac16e4b18df89b9a31d35550e16723d4815d
psyjd14 <psyjd14@nottingham.ac.uk>
2025-07-04 16:21:48 +0100

    complete revamp, make it a psuedo desktop

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 
 const hirebut = document.getElementById("hirebut");
 
+const tabs = document.querySelectorAll('.tab');
 
 document.addEventListener("mousemove", function(event) {
     hirebut.style.left = event.pageX - hirebut.offsetWidth/2 + "px";
     hirebut.style.top = event.pageY - hirebut.offsetHeight/2 + "px";
+});
+
+
+tabs.forEach(tab => {
+    tab.addEventListener('mousedown', (e) => {
+        let shiftX = e.clientX - tab.getBoundingClientRect().left;
+        let shiftY = e.clientY - tab.getBoundingClientRect().top;
+
+        function moveAt(pageX, pageY) {
+            tab.style.left = pageX - shiftX + 'px';
+            tab.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(e) {
+            moveAt(e.pageX, e.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        tab.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            tab.onmouseup = null;
+        };
+    });
+
+    tab.ondragstart = function() {
+        return false;
+    };
 });
